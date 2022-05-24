9aae254b82a47d73bcd0c96ce6ca023f23d450a3
LucielSantos <luciel.vitor.bs@gmail.com>
2022-05-24 01:44:46 -0300

    feat: adjustments to deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,16 +8,17 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Stardust',
   tagline: 'Biblioteca de componentes UI desenvolvido por Ubistart',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
+  url: 'https://LucielSantos.github.io',
+  baseUrl: '/test-docusaurus/',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'LucielSantos', // Usually your GitHub org/user name.
+  projectName: 'test-docusaurus', // Usually your repo name.
+  trailingSlash: true,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -40,9 +41,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars/index.js'),
           remarkPlugins: [require('./plugins/remark-snackplayer')],
         },
-        blog: {
-          showReadingTime: true,
-        },
+        blog: false,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
@@ -74,20 +73,14 @@ const config = {
         items: [
           { to: 'docs/react-js/getting-started', label: 'React JS', position: 'left' },
           { to: 'docs/react-native/getting-started', label: 'React Native', position: 'left' },
-          { to: '/blog', label: 'Core', position: 'left' },
+          { to: '/docs/react-js/getting-started', label: 'Core', position: 'left' },
         ],
       },
       footer: {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
+            title: 'Docs'
           },
           {
             title: 'Community',
@@ -109,10 +102,6 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
